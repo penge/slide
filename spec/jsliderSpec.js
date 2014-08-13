b34b24c6971ed1f71a10d2f8f99b47ad653ef9df
Pavel Bucka <pavelbucka@icloud.com>
2014-08-13 18:50:45 +0200

    Add SpecRunner and jsliderSpec

---
+++ spec/jsliderSpec.js
@@ -0,0 +1,29 @@
+describe('JSlider', function() {
+
+  describe('construction', function() {
+
+    it('properly sets variables', function() {
+      var jslider = new window.jslider({
+        id: 'my-news',
+        count: 15,
+        width: 200,
+        height: 100,
+        duration: 300,
+      });
+      expect(jslider.id).toBe('my-news');
+      expect(jslider.count).toBe(15);
+      expect(jslider.width).toBe(200);
+      expect(jslider.height).toBe(100);
+      expect(jslider.duration).toBe(300);
+    });
+    
+    it('properly sets defaults for variables', function() {
+      var jslider = new window.jslider();
+      expect(jslider.id).toBe('news');
+      expect(jslider.count).toBe(10);
+      expect(jslider.width).toBe(200);
+      expect(jslider.height).toBe(200);
+      expect(jslider.duration).toBe(100);
+    });
+  });
+});
