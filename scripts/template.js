84838ae38804504d7c07fc363a758eb4617c22a1
Pavel Bucka <pavelbucka@icloud.com>
2014-09-04 12:57:12 +0200

    Create Template object

---
+++ scripts/template.js
@@ -0,0 +1,75 @@
+window.Template = (function() {
+  'use strict';
+
+  var setId = function(id) {
+    $(this).find('.id').text(id);
+  };
+
+  var setInput = function(value) {
+    $(this).find('.input').attr('value', value);
+  };
+
+  var check = function() {
+    $(this).addClass('active');
+    $(this).find('.checkbox').prop('checked', true);
+    $(this).find('.input').prop('tabindex', null);
+  };
+
+  var uncheck = function() {
+    $(this).removeClass('active');
+    $(this).find('.checkbox').prop('checked', false);
+    $(this).find('.input').prop('tabindex', -1);
+  };
+
+  var toggleCheck = function(checked) {
+    if (checked) {
+      this.check();
+    } else {
+      this.uncheck();
+    }
+  };
+
+  var addMembers = function($template) {
+    $template.setId = setId;
+    $template.setInput = setInput;
+    $template.check = check;
+    $template.uncheck = uncheck;
+    $template.toggleCheck = toggleCheck;
+  };
+
+  var clone = function(templateId, idValue, inputValue, checked) {
+    var $element = $(templateId).clone();
+    return init($element, idValue, inputValue, checked);
+  };
+
+  var init = function($element, idValue, inputValue, checked) {
+    addMembers($element);
+    initId($element, idValue);
+    initInput($element, inputValue);
+    initChecked($element, checked);  
+    return $element;
+  };
+
+  var initId = function($element, idValue) {
+    if (idValue) {
+      $element.setId(idValue);
+    }
+  };
+
+  var initInput = function($element, inputValue) {
+    if (inputValue) {
+      $element.setInput(inputValue);
+    }
+  };
+
+  var initChecked = function($element, checked) {
+    if (checked === true || checked === false) {
+      $element.toggleCheck(checked);
+    }
+  };
+
+  return {
+    clone: clone,
+    init: init,
+  };
+})();
