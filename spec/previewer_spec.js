71b8a86b0859560bf459c740454d6fc98fb44f2a
Pavel Bucka <pavelbucka@icloud.com>
2014-09-07 01:03:24 +0200

    Simplify #getJs in previewer_spec
    * define slideArray and previewerArray

--- spec/previewer_spec.js
+++ spec/previewer_spec.js
@@ -6,21 +6,37 @@ describe('Previewer', function() {
   var slide = null;
   var previewer = null;
 
+  var slideArray = null;
+  var previewerArray = null;
+
   beforeEach(function() {
     slide = new Slide({
       id: 'news',
       count: 3,
       widths: 200,
+      width: 400,
       height: 250,
       duration: 100,
     });
+
+    slideArray = new Slide({
+      id: 'news',
+      count: 3,
+      widths: [200, 300, 400],
+      width: 600,
+      height: 250,
+      duration: 100,
+    });
+
     previewer = new Previewer(slide);  
+    previewerArray = new Previewer(slideArray);  
   });
 
   describe('init', function() {
 
     it('properly sets slide', function() {
       expect(previewer.slide.equals(slide)).toBe(true);  
+      expect(previewerArray.slide.equals(slideArray)).toBe(true);  
     });
   });
 
@@ -43,44 +59,33 @@ describe('Previewer', function() {
 
   describe('#getJs', function() {
 
-    describe('widths is Integer', function() {
+    it('returns correct js string', function() {
+      var expected = [
+        'var slide = new Slide({',
+        TAB + 'id: "news",',
+        TAB + 'count: 3,',
+        TAB + 'widths: 200,',
+        TAB + 'width: 400,',
+        TAB + 'height: 250,',
+        TAB + 'duration: 100,',
+        '});' + NL,
+        'slide.run();'
+      ].join(NL);
 
-      it('returns correct js string', function() {
-        var expected = [
-          'var slide = new Slide({',
-          TAB + 'id: "news",',
-          TAB + 'count: 3,',
-          TAB + 'widths: 200,',
-          TAB + 'height: 250,',
-          TAB + 'duration: 100,',
-          '});' + NL,
-          'slide.run();'
-        ].join(NL);
+      var expectedArray = [
+        'var slide = new Slide({',
+        TAB + 'id: "news",',
+        TAB + 'count: 3,',
+        TAB + 'widths: [200, 300, 400],',
+        TAB + 'width: 600,',
+        TAB + 'height: 250,',
+        TAB + 'duration: 100,',
+        '});' + NL,
+        'slide.run();'
+      ].join(NL);
 
-        expect(previewer.getJs()).toEqual(expected);
-      });
-    });
-
-    describe('widths is Array', function() {
-
-      beforeEach(function() {
-        slide.setWidths([200, 300, 400]);
-      });
-
-      it('returns correct js string', function() {
-        var expected = [
-          'var slide = new Slide({',
-          TAB + 'id: "news",',
-          TAB + 'count: 3,',
-          TAB + 'widths: [200, 300, 400],',
-          TAB + 'height: 250,',
-          TAB + 'duration: 100,',
-          '});' + NL,
-          'slide.run();'
-        ].join(NL);
-
-        expect(previewer.getJs()).toEqual(expected);
-      });
+      expect(previewer.getJs()).toEqual(expected);
+      expect(previewerArray.getJs()).toEqual(expectedArray);
     });
   });
 });
